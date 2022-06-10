c17f3d8f450b99120e705c46d935f9e9431b29a5
miku <svartkatt39@gmail.com>
2022-06-10 14:59:49 +0900

    implement reset

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 console.log("hello world");
 
-let result, timer;
+let timer;
 
 const startButton = document.getElementById("startButton");
 startButton.addEventListener("click", startTimer);
@@ -9,51 +9,67 @@ startButton.addEventListener("click", startTimer);
 const stopButton = document.getElementById("stopButton");
 stopButton.addEventListener("click", stopTimer);
 
+const clearButton = document.getElementById("clearButton");
+clearButton.addEventListener("click", clearTimer);
+
 function startTimer() {
-  let ss, mm, counter, targetSec;
+  let ss, mm, targetSec, result;
+  let counter = 0;
 
   targetSec = 120;
-  mm = 2;
+  mm = 0;
   ss = 10;
-  counter =0; 
+
+  //もしタイマーが再開であれば、経過した時間から開始する
+  let startTime = document.getElementById("result").textContent.split(":");
+  if (parseInt(startTime[2]) > 0) {
+    mm = startTime[1];
+    ss = startTime[2];
+  }
 
   timer = window.setInterval(() => {
     if (timer !== null && targetSec > 0) {
-      ss = parseInt(ss) < 10 ? "0" + ss : ss;
-      //   console.log({ ss });
-      mm = parseInt(mm) < 10 ? "0" + mm : mm;
-      console.log({ mm });
+      ss = parseInt(ss);
+      mm = parseInt(mm);
+      ss = ss < 10 && ss >= 0 ? "0" + ss : ss;
+      mm = mm < 10 && mm >= 0 ? "0" + mm : mm;
     }
     result = document.getElementById("result");
     result.textContent = `00:${mm}:${ss}`;
 
-    //もし秒がゼロになったらリセットする
-    if (parseInt(ss) === 0) {
+    ss = parseInt(ss);
+    mm = parseInt(mm);
+
+    //秒がゼロになったら60に初期化する
+    if (mm > 0 && ss === 0) {
       ss = 60;
       counter += 1;
-      console.log('hello')
-      console.log({counter})
+      //counterで分の経過を計算する
       if (counter > 0) {
-          console.log('increment a minute')
-        mm = parseInt(mm);
-        console.log("??", { mm });
         mm -= counter;
       }
+    } else if (mm === 0 && ss === 0) {
+      console.log("fin");
+      stopTimer();
+      return;
     }
-    ss = parseInt(ss);
     ss--;
-    mm = parseInt(mm); 
+    console.log({ mm });
     console.log({ ss });
     targetSec--;
   }, 1000);
 }
 
 function stopTimer() {
-  console.log("stop!!");
-
-  //IDを取り消す
   window.clearInterval(timer);
   timer = null;
+}
+
+//setIntervalID, タイマーを初期化
+function clearTimer() {
+  window.clearInterval(timer);
+  timer = null;
+  document.getElementById("result").textContent = "00:00:00";
 }
 
 /*

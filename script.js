883b01c8c82301005c76dc0cae063aff7f40e0c2
miku <svartkatt39@gmail.com>
2022-06-10 16:36:19 +0900

    refactor message and clock

--- script.js
+++ script.js
@@ -12,21 +12,19 @@ stopButton.addEventListener("click", stopTimer);
 const clearButton = document.getElementById("clearButton");
 clearButton.addEventListener("click", clearTimer);
 
-
 function startTimer() {
   let ss, mm, targetSec, result;
   let counter = 0;
 
-  mm = document.body.querySelector("input").value; 
-  if(!mm){
-    console.log("please set minutes"); 
-    return; 
+  //入力値から値を取得する
+  mm = document.body.querySelector("input").value;
+  if (!mm) {
+    console.log("please set minutes");
+    return;
   }
-  mm = parseInt(mm); 
-  // console.log({mm}); 
-  ss = 0; 
-  targetSec = mm * 60; 
-
+  mm = parseInt(mm);
+  ss = 0;
+  targetSec = mm * 60;
 
   //もしタイマーが再開であれば、経過した時間から開始する
   let startTime = document.getElementById("result").textContent.split(":");
@@ -34,6 +32,10 @@ function startTimer() {
     mm = startTime[1];
     ss = startTime[2];
   }
+  console.log("startTime mm:", mm);
+  console.log("startTime ss:", ss);
+  console.log("message mm:", mm);
+  message(parseInt(mm));
 
   timer = window.setInterval(() => {
     if (timer !== null && targetSec > 0) {
@@ -48,7 +50,7 @@ function startTimer() {
     ss = parseInt(ss);
     mm = parseInt(mm);
 
-    //秒がゼロになったら60に初期化する
+    //秒がゼロになったら60に初期化し分を1繰り下げる
     if (mm > 0 && ss === 0) {
       ss = 60;
       counter += 1;
@@ -60,11 +62,12 @@ function startTimer() {
     } else if (mm === 0 && ss === 0) {
       console.log("fin");
       stopTimer();
+      document.getElementById("result").textContent = "00:00:00";
       return;
       //残り10秒になったら1秒ごとに背景の色を切り替える
-    } else if ( mm === 0 && ss < 10 && ss % 2 === 0 ){
-      document.body.style.background = "orange";  
-    } else if (mm === 0 && ss < 10 && ss % 2 === 1){
+    } else if (mm === 0 && ss < 10 && ss % 2 === 0) {
+      document.body.style.background = "orange";
+    } else if (mm === 0 && ss < 10 && ss % 2 === 1) {
       document.body.style.background = "white";
     }
     ss--;
@@ -72,14 +75,11 @@ function startTimer() {
     // console.log({ ss });
     targetSec--;
   }, 1000);
-
-  message(); 
 }
 
 function stopTimer() {
   window.clearInterval(timer);
   timer = null;
-  document.getElementById("result").textContent = "00:00:00";
 }
 
 //setIntervalID, タイマーとメッセージを初期化
@@ -87,39 +87,33 @@ function clearTimer() {
   window.clearInterval(timer);
   timer = null;
   document.getElementById("result").textContent = "00:00:00";
-  document.body.querySelector("input").value = ""; 
-  document.getElementById('message').textContent = " "; 
+  document.body.querySelector("input").value = "";
+  document.getElementById("message").textContent = " ";
 }
 
-function message(){
-  let breakTime = document.body.querySelector("input").value; 
-  breakTime = parseInt(breakTime); 
+function message(breakTime) {
+  let now = new Date();
+  let hh = now.getHours();
+  let mm = now.getMinutes();
 
-  let now = new Date; 
-  let hh = now.getHours(); 
-  let mm = now.getMinutes(); 
-
-  mm = mm + breakTime; 
-  let counter = 0; 
-  while(mm > 59){
-    mm-=60;
-    counter++; 
+  mm = mm + breakTime;
+  let counter = 0;
+  while (mm > 59) {
+    mm -= 60;
+    counter++;
   }
-  if(counter > 0){
-    hh += counter; 
+  if (counter > 0) {
+    hh += counter;
   }
-  console.log({hh})
-  console.log({mm})
+  // console.log({hh})
+  // console.log({mm})
 
+  hh = hh < 10 ? "0" + hh : hh;
+  mm = mm < 10 ? "0" + mm : mm;
 
-  hh = hh < 10 ?  "0" + hh : hh; 
-  mm = mm < 10 ?  "0" + mm : mm;
-
-  let message = document.getElementById('message'); 
-  message.textContent = `休憩時間は ${hh}:${mm} までです`
+  let message = document.getElementById("message");
+  message.textContent = `休憩時間は ${hh}:${mm} までです`;
 }
-
-  
 
 /*
 参考
